4e01490526a69e1e784de047dfd58bf40f601a3c
fifi000 <filipkociok@gmail.com>
2025-05-11 22:16:26 +0200

    more todos

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-
 from core.enums.language import Language
 from core.game import Game
 from core.player import Player
@@ -10,27 +9,29 @@ def main() -> None:
         Player('Filip'),
         Player('Zuzia'),
     ]
-    
+
     game = Game(players, Language.POLISH)
 
     while True:
         current_player = game.current_player
 
         print(f"\n[# {game.move}] It's player {current_player.name} turn!")
-        current_letters = ' '.join([letter.symbol for letter in current_player.letters])
+        current_letters = ' '.join([letter.symbol for letter in current_player.tiles])
         print(f'Current letters: "{current_letters}"')
 
         word = input('Word: ').strip().upper()
 
-        available_letters = current_player.letters.copy()
+        available_letters = current_player.tiles.copy()
         letters = []
 
         for char in word:
             letter = next((x for x in available_letters if x.symbol == char))
             letters.append(letter)
 
-        game.place_letters(current_player, [(Position(0, 0), letter) for letter in letters])
+        game.place_letters(
+            current_player, [(Position(0, 0), letter) for letter in letters]
+        )
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     main()
--- todo.md
+++ todo.md
@@ -5,3 +5,10 @@
 - [ ] UI language selection
 - [ ] AI oponent, based on read text
 - [ ] undo / redo
+
+<!-- UI -->
+
+- [ ] add opacity to field bg colors
+- [ ] add background color to placed tiles
+- [ ] allow user to sort tiles (only horizontally)
+- [ ] make a browser app (for phones) to work as a controller
